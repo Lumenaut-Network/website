51898a40b5a8c60c88d9696e1a9564f492ee5267
iduvall <iduvall@umich.edu>
2018-01-12 15:31:51 -0600

    Remove EUR ticker and add ETH.

--- static/price-ticker.js
+++ static/price-ticker.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
     //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
     //For aggregate quote updates use CCCAGG as market
-    var subs = ['5~CCCAGG~XLM~BTC', '5~CCCAGG~XLM~USD', '5~CCCAGG~XLM~EUR'];
+    var subs = ['5~CCCAGG~XLM~USD', '5~CCCAGG~XLM~BTC', '5~CCCAGG~XLM~ETH'];
     socket.emit('SubAdd', { subs });
     socket.on("m", function(message) {
         var messageType = message.substring(0, message.indexOf("~"));
