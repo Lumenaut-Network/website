ac9c07565e91a10435da7702e302481b33cec047
Lumenaut <33041885+Lumenaut@users.noreply.github.com>
2018-02-20 17:30:47 +0000

    Update and rename custom.js to global.js

---
+++ assets/js/global.js
@@ -0,0 +1,384 @@
+// fetch vote info
+$(document).ready(() => {
+	// pool address
+	const inflationDest = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+	$.get({
+		url: 'https://fed.network/inflation/' + inflationDest
+	}).then((result) => {
+		$('#accounts-contributing').text(result.entries.length.toLocaleString());
+		const totalVotes = result.entries.reduce((sum, entry) => {
+			return sum + parseInt(entry.balance) / 10000000;
+		}, 0);
+		$('#total-votes').text(parseInt(totalVotes).toLocaleString());
+	}, (err) => {
+		console.log('error: ', err.responseJSON);
+	});
+});
+
+
+
+// clipboard
+$(document).ready(() => {
+	var clipboard = new Clipboard('.addr a');
+	const $inflation = $('#addrInflation a');
+	const $donation = $('#addrDonation a');
+
+	$inflation.click((e) => {
+		e.preventDefault();
+		$inflation.addClass('visited');
+	});
+
+	$donation.click((e) => {
+		e.preventDefault();
+		$donation.addClass('visited');
+	});
+});
+
+
+
+// payments database
+$(document).ready(function(){
+	var table = $('#pool').DataTable({
+		'paging':   false,
+		'info':     false,
+		'order': [[ 0, "desc" ]],
+		'ajax': 'https://lumenaut-network.github.io/website/payments/arrays.txt',
+		'columnDefs': [ {
+			'targets': -1,
+			'data': null,
+			'defaultContent': '<a class="result"><svg style="fill:currentColor;" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M312 196v24c0 6.6-5.4 12-12 12h-68v68c0 6.6-5.4 12-12 12h-24c-6.6 0-12-5.4-12-12v-68h-68c-6.6 0-12-5.4-12-12v-24c0-6.6 5.4-12 12-12h68v-68c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v68h68c6.6 0 12 5.4 12 12zm196.5 289.9l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L347.5 387.1c-2.3-2.3-3.5-5.3-3.5-8.5v-13.2c-36.5 31.5-84 50.6-136 50.6C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208c0 52-19.1 99.5-50.6 136h13.2c3.2 0 6.2 1.3 8.5 3.5l121.4 121.4c4.7 4.7 4.7 12.3 0 17zM368 208c0-88.4-71.6-160-160-160S48 119.6 48 208s71.6 160 160 160 160-71.6 160-160z"></path></svg></a>'
+		}]
+	});
+
+	table.on( 'click', '.result', function() {
+		var data = table.row($(this).parents('tr')).data();
+		window.open(data[ 5 ], '_blank');
+	});
+
+
+	$('#pool_filter input').attr("placeholder", "search...");
+});
+
+
+
+// calculator
+$(document).ready(function() {
+
+	// Taken from https://stackoverflow.com/questions/1909441/how-to-delay-the-keyup-handler-until-the-user-stops-typing
+	var delay = (function(){
+		var timer = 0;
+		return function(callback, ms){
+			clearTimeout (timer);
+			timer = setTimeout(callback, ms);
+		};
+	})();
+
+	var userXLM;
+	var totalCoins;
+	var feePool;
+	var $calcInput = $('.calculator input');
+	var $calcResult = $('.calculator span');
+	const server = new StellarSdk.Server('https://horizon.stellar.org');
+	StellarSdk.Network.usePublicNetwork();
+
+	var getXLMAmount = function(xlmOrAddress) {
+		return new Promise(function(resolve, reject){
+			// check for XLM amount or valid address
+			userXLM = parseFloat(xlmOrAddress);
+			if (!isNaN(userXLM)) {
+				resolve(userXLM);
+			} else if (StellarSdk.StrKey.isValidEd25519PublicKey(xlmOrAddress)) {
+				var userAddress = xlmOrAddress;
+				server.loadAccount(userAddress)
+				.then((account) => {
+					console.log(account);
+					const nativeBalance = account.balances.find((el) => {return el.asset_type === 'native';});
+					userXLM = nativeBalance.balance;
+					resolve(userXLM);
+				}, (fail) => {
+					console.log('failed to fetch user account in calculator - error: ', fail);
+					reject();
+				});
+			} else {
+				// not a valid XLM amount or valid address
+				reject();
+			}
+		});
+
+	};
+
+	var getLatestLedgerInfo = function() {
+		return new Promise(function(resolve, reject){
+			if (totalCoins && feePool) {
+				// We got the value cached, no need to hit the API
+				resolve();
+			} else {
+
+				// Hit the API
+				server.ledgers().limit(1).order('desc').call()
+				.then(({records}) => {
+					feePool = records[0].fee_pool;
+					totalCoins = records[0].total_coins;
+					resolve();
+				}, (fail) => {
+					console.log('failed to fetch ledger in calculator - errror: ', fail);
+					reject();
+				});
+			}
+		});
+	};
+
+	// Our main listener
+	$calcInput.keyup(function() {
+		delay(function(){
+			// 1. Get XLM Amount AND get totalCoin + feePool
+			// 2. Calculate inflation
+			// 3. Update
+			Promise.all([getXLMAmount($calcInput.val().replace(/,/g, '')), getLatestLedgerInfo()])
+			.then(function([XLMAmount, _]){
+				// weekly inlation rate set in stellar core code - https://github.com/stellar/stellar-core/blob/master/src/transactions/InflationOpFrame.cpp
+				var total = Math.max((XLMAmount * 0.000190721) + (userXLM * feePool / totalCoins ) - 0.0000100, 0.0);
+
+				// This is not accurate and also slowwwww
+				$calcResult.html('Estimated payout = <strong>' + Number((total).toFixed(7)) + '</strong> XLM');
+			})
+			.catch(function(e){
+				console.log("Please check your address / XLM amount!");
+				$calcResult.html(0);
+			});
+		}, 400 );
+  	});
+});
+
+
+
+// i18n
+$(document).ready(() => {
+	var translate = null;
+
+	// Method to translate into the specified language
+	function translateInto(lang) {
+		// Getting all the nodes having a 'data-i18n' attribute
+		const nodes = document.querySelectorAll('[data-i18n]');
+
+		// Fetching the matched JSON translation file
+		$.get({
+			url: './assets/js/i18n/' + lang + '.json'
+		}).then((data) => {
+			const languageFile = JSON.parse(data)
+			translate = i18n.create({
+				values: languageFile
+			});
+		
+			// Looping through the nodes to replace the [data-i18n]
+			// attribute string into the translation
+			for (const node of nodes) {
+				const key = node.attributes['data-i18n'].value;
+				const translation = translate(key); // to replace
+				if (translation) {
+					node.innerHTML = translation;
+				}
+			}
+		});
+	}
+
+	$('.lang .en').click(function() {
+		translateInto('en_US');
+	});
+
+	$('.lang .fr').click(function() {
+		translateInto('fr_FR');
+	});
+
+	$('.lang .es').click(function() {
+		translateInto('es_ES');
+	});
+});
+
+
+
+// price ticker
+$(document).ready(function() {
+
+    var currentPrice = {};
+    var socket = io.connect('https://streamer.cryptocompare.com/');
+    //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
+    //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
+    //For aggregate quote updates use CCCAGG as market
+    var subs = ['5~CCCAGG~XLM~USD', '5~CCCAGG~XLM~BTC', '5~CCCAGG~XLM~ETH'];
+    socket.emit('SubAdd', { subs });
+    socket.on("m", function(message) {
+        var messageType = message.substring(0, message.indexOf("~"));
+        var res = {};
+        if (messageType == CCC.STATIC.TYPE.CURRENTAGG) {
+            res = CCC.CURRENT.unpack(message);
+            dataUnpack(res);
+        }
+    });
+
+    var dataUnpack = function(data) {
+        var fromSym = data['FROMSYMBOL'];
+        var toSym = data['TOSYMBOL'];
+        var fsym = CCC.STATIC.CURRENCY.getSymbol(fromSym);
+        var tsym = CCC.STATIC.CURRENCY.getSymbol(toSym);
+        var pair = fromSym + '-' + toSym;
+
+        if (!currentPrice.hasOwnProperty(pair)) {
+            currentPrice[pair] = {};
+        }
+
+        for (var key in data) {
+            currentPrice[pair][key] = data[key];
+        }
+
+        if (currentPrice[pair]['LASTTRADEID']) {
+            currentPrice[pair]['LASTTRADEID'] = parseInt(currentPrice[pair]['LASTTRADEID']).toFixed(0);
+        }
+        currentPrice[pair]['CHANGE24HOUR'] = CCC.convertValueToDisplay(tsym, (currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']));
+        currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";;
+        displayData(currentPrice[pair], fromSym, toSym, pair);
+    };
+
+    var displayData = function(current, fromSym, toSym, pairSym) {
+        // console.log('current: ', current, fromSym, toSym, pairSym, CCC.STATIC.CURRENCY.getSymbol(toSym));
+
+        $('#price_' + pairSym).text(current.PRICE);
+        $('#change24HourPct_' + pairSym).text(' (' + current.CHANGE24HOURPCT + ')');
+
+        var priceDirection = current.FLAGS;
+        $('#price_' + pairSym).removeClass();
+        if (priceDirection & 1) {
+            $('#price_' + pairSym).addClass("highlight-green");
+        }
+        else if (priceDirection & 2) {
+            $('#price_' + pairSym).addClass("highlight-red");
+        }
+        if (current['PRICE'] > current['OPEN24HOUR']) {
+            $('#change24HourPct_' + pairSym).removeClass();
+            $('#change24HourPct_' + pairSym).addClass("price_up");
+        }
+        else if (current['PRICE'] < current['OPEN24HOUR']) {
+            $('#change24HourPct_' + pairSym).removeClass();
+            $('#change24HourPct_' + pairSym).addClass("price_down");
+        }
+    };
+});
+
+
+
+// close button
+$(document).on('click', '.close', function (e) {
+    e.preventDefault();
+    document.location.hash = "";
+});
+
+
+
+// Setup & Instructions
+$(document).ready(function() {
+	// Lumenaut Community Pool address
+	const POOL_ADDRESS = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+	const $castVoteTab = $('#v-pills-cast-vote-tab');
+	const $instructions = $('.instructions');
+	const $walletLogos = $('.wallet-logos');
+	const $addrInput = $('#verify-xlm-address-input');
+	const $verifyButton = $('#verify-xlm-address-button');
+	const $verifyResult = $('#verify-xlm-address-result');
+
+	const instructionsState = {};
+	const wallet_to_custom_instructions = {
+		'stellar-desktop-client': 'stellar-desktop-client-instructions',
+		'ledger': 'ledger-nano-instructions'
+	};
+
+	const server = new StellarSdk.Server('https://horizon.stellar.org');
+	StellarSdk.Network.usePublicNetwork();
+
+	// listen for wallet logo clicks
+	$walletLogos.click((e) => {
+		e.preventDefault();
+
+		const selected_wallet = e.target.dataset['wallet'];
+		instructionsState['selected_wallet'] = selected_wallet;
+		instructionsState['instructions'] = wallet_to_custom_instructions[selected_wallet] || 'stellar-laboratory-instructions';
+		console.log(selected_wallet, instructionsState.instructions);
+
+		$instructions.hide();
+		$('#' + instructionsState.instructions).show();
+
+		$castVoteTab.click();
+	});
+
+
+	$addrInput.keyup((e) => {
+		e.preventDefault();
+
+		$verifyResult.empty();
+
+		const pubKey = $addrInput.val();
+		if (pubKey && StellarSdk.StrKey.isValidEd25519PublicKey(pubKey)) {
+			verifyAccount();
+		}
+	});
+
+	$verifyButton.click(verifyAccount);
+
+	function verifyAccount() {
+
+		const pubKey = $addrInput.val();
+		console.log('pub key: ', pubKey);
+
+		if (!StellarSdk.StrKey.isValidEd25519PublicKey(pubKey)) {
+			$verifyResult.empty();
+			$verifyResult.append(
+				"<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">\
+					" + translate('site.main.join.step_4.error.bad_format') + "\
+					<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\
+						<span aria-hidden=\"true\">&times;</span>\
+					</button>\
+				</div>"
+			);
+			return;
+		}
+
+		try {
+			server.loadAccount(pubKey).then((account) => {
+				console.log(account);
+
+				if (account.inflation_destination != POOL_ADDRESS) {
+					$verifyResult.empty();
+					$verifyResult.append(
+						"<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">\
+							" + translate('site.main.join.step_4.error.not_joined') + " \
+							<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\
+								<span aria-hidden=\"true\">&times;</span>\
+							</button>\
+						</div>"
+					);
+				} else {
+					$verifyResult.empty();
+					$verifyResult.append(
+						"<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">\
+							" + translate('site.main.join.step_4.success') + "\
+							<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\
+								<span aria-hidden=\"true\">&times;</span>\
+							</button>\
+						</div>"
+					);
+				}
+			});
+		} catch(err) {
+			console.log(err);
+			$verifyResult.empty();
+			$verifyResult.append(
+				"<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">\
+					" + translate('site.main.join.step_4.error.fetching') + "\
+					<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\
+						<span aria-hidden=\"true\">&times;</span>\
+					</button>\
+				</div>"
+			);
+			return;
+		}
+	}
+});
