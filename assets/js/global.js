c8b39a2e0a1114494f55ab83035b1333562d6394
Lumenaut <33041885+Lumenaut@users.noreply.github.com>
2018-03-12 04:26:25 +0000

    Fixes and customisation.

--- assets/js/global.js
+++ assets/js/global.js
@@ -197,125 +197,6 @@ $(document).ready(() => {
 
 
 
-// transaction export
-$(document).ready(function() {
-	var loadedPayments = [];
-
-	document.getElementById("get").onclick = function () {
-
-		sourceAddress = GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT;
-		destinationAddress = document.getElementById('destinationAddress').value;
-
-		if (!destinationAddress) {
-			console.error("Destination Address is missing!");
-			return;
-		}
-
-		var timeFrameValue = document.getElementById("timeframe").value;
-
-		endDate = moment();
-		startDate = moment().subtract(30, 'days').startOf('day');
-
-		switch (timeFrameValue) {
-			case 'year':
-				startDate = moment().subtract(1, 'year').startOf('day');
-				break;
-			case 'prevYear':
-				endDate = moment().startOf('year');
-				timeFrameValue
-				startDate = moment().startOf('year').subtract(1, 'year');
-				break;
-		}
-
-		var url = 'https://horizon.stellar.org/accounts/' + destinationAddress + '/payments?limit=200&order=desc'
-
-		getData(url, sourceAddress, startDate, endDate, function (payments) {
-			loadedPayments = payments;
-			var dataTable = document.getElementById("data");
-			dataTable.innerHTML = '';
-
-			payments.forEach(function (payment) {
-				dataTable.innerHTML += `<tr><td>${payment.date.format()}</td><td>${payment.amount}</td></tr>`;
-			});
-
-			document.getElementById("export").disabled = false;
-		});
-	}
-
-	document.getElementById("export").onclick = function () {
-		var timeFrameValue = document.getElementById("timeframe").value;
-		var sourceAddress = GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT;
-
-		//create csv
-		var csvFile = 'Date,Amount(XLM)';
-		loadedPayments.forEach(function (payment) {
-			csvFile += '\n' + payment.date.format() + ',' + payment.amount;
-		})
-
-		//download as file
-		var a = document.createElement('a');
-		var file = new Blob([csvFile], {
-			type: 'csv'
-		});
-		a.href = URL.createObjectURL(file);
-		a.download = `${sourceAddress.substring(0,5)}_${timeFrameValue}_PaymentsExport.csv`;
-		a.click();
-	}
-
-	function getData(url, sourceAddress, startDate, endDate, callback) {
-
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function () {
-			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-				var payments = JSON.parse(xmlHttp.responseText);
-				processPayments(payments, sourceAddress, startDate, endDate, callback);
-			}
-			else if (xmlHttp.status == 404) {
-				callback([]);
-			}
-		}
-
-		xmlHttp.open("GET", url, true);
-		xmlHttp.send(null);
-	}
-
-	function processPayments(payments, sourceAddress, startDate, endDate, callback) {
-		var paymentsOfInterest = [];
-		var lastPaymentDate;
-		var records = payments['_embedded'].records;
-
-		if (records.length == 0) {
-			callback(paymentsOfInterest);
-			return;
-		}
-
-		records.forEach(function (payment) {
-			lastPaymentDate = moment(payment['created_at']);
-
-			if (!lastPaymentDate.isBetween(startDate, endDate)) {
-				return;
-			}
-
-			if (payment['source_account'] == sourceAddress) {
-				paymentsOfInterest.push({
-				date: lastPaymentDate,
-				amount: payment.amount
-			});
-		}
-	});
-
-	if (lastPaymentDate.isAfter(startDate)) {
-		getData(payments['_links'].prev.href, sourceAddress, startDate, endDate, function (newPayments) {
-			callback(paymentsOfInterest.concat(newPayments));
-		});
-	}
-	else {
-        	callback(paymentsOfInterest);
-	}
-});
-
-
-
 // price ticker
 $(document).ready(function() {
 
@@ -487,3 +368,233 @@ $(document).ready(function() {
 		}
 	}
 });
+
+
+
+// lumenaut payments export
+
+var loadedPayments = [];
+
+document.getElementById("get").onclick = function () {
+
+	lumenaut = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+	address = document.getElementById('address').value;
+
+	if (!address) {
+		console.error("Your Public Key is missing!");
+		return;
+	}
+
+	var timeFrameValue = document.getElementById("timeframe").value;
+	endDate = moment();
+	startDate = moment().subtract(30, 'days').startOf('day');
+
+	switch (timeFrameValue) {
+		case 'year':
+			startDate = moment().subtract(1, 'year').startOf('day');
+			break;
+		case 'prevYear':
+			endDate = moment().startOf('year');
+			timeFrameValue
+			startDate = moment().startOf('year').subtract(1, 'year');
+			break;
+	}
+
+	var url = 'https://horizon.stellar.org/accounts/' + address + '/payments?limit=200&order=desc'
+
+	getData(url, lumenaut, startDate, endDate, function (payments) {
+		loadedPayments = payments;
+		var dataTable = document.getElementById("data");
+		dataTable.innerHTML = '';
+
+		payments.forEach(function (payment) {
+			dataTable.innerHTML += `<tr><td>${payment.date.format()}</td><td>${payment.amount}</td></tr>`;
+		});
+
+		document.getElementById("csv").disabled = false;
+	});
+}
+
+document.getElementById("csv").onclick = function () {
+	var timeFrameValue = document.getElementById("timeframe").value;
+	var lumenaut = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+
+	//create csv
+	var csvFile = 'Date,Amount(XLM)';
+	loadedPayments.forEach(function (payment) {
+		csvFile += '\n' + payment.date.format() + ',' + payment.amount;
+	})
+
+	//download as file
+	var a = document.createElement('a');
+	var file = new Blob([csvFile], {
+		type: 'csv'
+	});
+	a.href = URL.createObjectURL(file);
+	a.download = `${lumenaut.substring(0,5)}_${timeFrameValue}_PaymentsExport.csv`;
+	a.click();
+}
+
+function getData(url, lumenaut, startDate, endDate, callback) {
+
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			var payments = JSON.parse(xmlHttp.responseText);
+			processPayments(payments, lumenaut, startDate, endDate, callback);
+		}
+		else if (xmlHttp.status == 404) {
+			callback([]);
+		}
+	}
+
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send(null);
+}
+
+function processPayments(payments, lumenaut, startDate, endDate, callback) {
+	var paymentsOfInterest = [];
+	var lastPaymentDate;
+	var records = payments['_embedded'].records;
+
+	if (records.length == 0) {
+		callback(paymentsOfInterest);
+		return;
+	}
+
+	records.forEach(function (payment) {
+		lastPaymentDate = moment(payment['created_at']);
+
+		if (!lastPaymentDate.isBetween(startDate, endDate)) {
+			return;
+		}
+
+		if (payment['source_account'] == lumenaut) {
+
+			paymentsOfInterest.push({
+				date: lastPaymentDate,
+				amount: payment.amount
+			});
+		}
+	});
+
+	if (lastPaymentDate.isAfter(startDate))
+		getData(payments['_links'].prev.href, lumenaut, startDate, endDate, function (newPayments) {
+			callback(paymentsOfInterest.concat(newPayments));
+	});
+	else
+		callback(paymentsOfInterest);
+}
+
+var loadedPayments = [];
+
+document.getElementById("get").onclick = function () {
+
+	lumenaut = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+	address = document.getElementById('address').value;
+
+	if (!address) {
+		console.error("Your Public Key is missing!");
+		return;
+	}
+
+	var timeFrameValue = document.getElementById("timeframe").value;
+	endDate = moment();
+	startDate = moment().subtract(30, 'days').startOf('day');
+
+	switch (timeFrameValue) {
+		case 'year':
+			startDate = moment().subtract(1, 'year').startOf('day');
+			break;
+		case 'prevYear':
+			endDate = moment().startOf('year');
+			timeFrameValue
+			startDate = moment().startOf('year').subtract(1, 'year');
+			break;
+	}
+
+	var url = 'https://horizon.stellar.org/accounts/' + address + '/payments?limit=200&order=desc'
+
+	getData(url, lumenaut, startDate, endDate, function (payments) {
+		loadedPayments = payments;
+		var dataTable = document.getElementById("data");
+		dataTable.innerHTML = '';
+
+		payments.forEach(function (payment) {
+			dataTable.innerHTML += `<tr><td>${payment.date.format()}</td><td>${payment.amount}</td></tr>`;
+		});
+
+		document.getElementById("csv").disabled = false;
+	});
+}
+
+document.getElementById("csv").onclick = function () {
+	var timeFrameValue = document.getElementById("timeframe").value;
+	var lumenaut = 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT';
+
+	//create csv
+	var csvFile = 'Date,Amount(XLM)';
+	loadedPayments.forEach(function (payment) {
+		csvFile += '\n' + payment.date.format() + ',' + payment.amount;
+	})
+
+	//download as file
+	var a = document.createElement('a');
+	var file = new Blob([csvFile], {
+		type: 'csv'
+	});
+	a.href = URL.createObjectURL(file);
+	a.download = `${lumenaut.substring(0,5)}_${timeFrameValue}_PaymentsExport.csv`;
+	a.click();
+}
+
+function getData(url, lumenaut, startDate, endDate, callback) {
+
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			var payments = JSON.parse(xmlHttp.responseText);
+			processPayments(payments, lumenaut, startDate, endDate, callback);
+		}
+		else if (xmlHttp.status == 404) {
+			callback([]);
+		}
+	}
+
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send(null);
+}
+
+function processPayments(payments, lumenaut, startDate, endDate, callback) {
+	var paymentsOfInterest = [];
+	var lastPaymentDate;
+	var records = payments['_embedded'].records;
+
+	if (records.length == 0) {
+		callback(paymentsOfInterest);
+		return;
+	}
+
+	records.forEach(function (payment) {
+		lastPaymentDate = moment(payment['created_at']);
+
+		if (!lastPaymentDate.isBetween(startDate, endDate)) {
+			return;
+		}
+
+		if (payment['source_account'] == lumenaut) {
+
+			paymentsOfInterest.push({
+				date: lastPaymentDate,
+				amount: payment.amount
+			});
+		}
+	});
+
+	if (lastPaymentDate.isAfter(startDate))
+		getData(payments['_links'].prev.href, lumenaut, startDate, endDate, function (newPayments) {
+			callback(paymentsOfInterest.concat(newPayments));
+	});
+	else
+		callback(paymentsOfInterest);
+}
